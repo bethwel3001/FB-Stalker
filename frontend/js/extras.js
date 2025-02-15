75acd74c151d71d93a941353a4f502da399514f3
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-02-15 18:10:19 +0300

    Light/dark and Navbar added

---
+++ frontend/js/extras.js
@@ -0,0 +1,10 @@
+
+function toggleMenu() {
+    const menuItems = document.querySelector('.menu-items');
+    menuItems.classList.toggle('active');
+  }
+  
+  function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+  }
