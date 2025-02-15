7d392b3337c399702ad9c26585b71f8b5d634d26
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-02-15 17:30:52 +0300

    Frontend skeleton done

---
+++ frontend/js/script.js
@@ -0,0 +1,5 @@
+// navigation
+document.querySelector('.continue-btn').addEventListener('click', () => {
+    alert('Redirecting to the next page...');
+    window.location.href = 'features.html'; 
+  });
